e0e4a8b9e68eef5ea207a60f3a000217429e1e74
gfortune1 <fgeee1@gmail.com>
2023-02-15 22:34:05 +0000

    created charactersarray, get character function plus card component

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,21 @@
 const getState = ({ getStore, getActions, setStore }) => {
+	
+	let apiUrl="https://swapi.dev/api/"
+	
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			characters:[]
+			
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getCharacters:()=>{
+				fetch(apiUrl+"people").then(resp=>resp.json()).then(data=>setStore({characters:data.results}))
+			}
+			
+				
 			}
 		}
 	};
-};
+
 
 export default getState;
