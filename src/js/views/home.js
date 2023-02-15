6e09e201cc2a12b0b27a931a00632d24f498e94e
gfortune1 <fgeee1@gmail.com>
2023-02-15 22:36:19 +0000

    updated home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React,{useContext} from "react";
+import {Card} from "../component/card"
+import { Context } from "../store/appContext";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		
-	</div>
-);
+export const Home = () => {
+	const {store,actions}=useContext(Context)
+	return(<div className="text-center mt-5">
+	<div>
+		<h1>Characters</h1>
+		<div>
+
+		</div>
+		</div>	
+	</div>)
+};
