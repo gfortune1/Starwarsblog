b8c06a15dcdfabb3de5b731f78ff1793cf872845
gfortune1 <fgeee1@gmail.com>
2023-02-16 22:29:05 +0000

    properties

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,17 +1,23 @@
-import React,{useContext} from "react";
-import {Card} from "../component/card"
+import React, { useContext } from "react";
+import { Card } from "../component/card";
 import { Context } from "../store/appContext";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const {store,actions}=useContext(Context)
-	return(<div className="text-center mt-5">
-	<div>
-		<h1>Characters</h1>
-		<div>
-
-		</div>
-		</div>	
-	</div>)
+  const { store, actions } = useContext(Context);
+  return (
+    <div className="text-center mt-5">
+      <div>
+        <h1>Characters</h1>
+        <div className="row">
+         {store.characters.map((character,index)=>{
+          return ( <div className="col">
+          <Card item= {character} index={index}/>
+        </div>)
+         })}
+        </div>
+      </div>
+    </div>
+  );
 };
